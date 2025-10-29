f88fec818d465d6266db0a48c696faa1b8d2c4e4
RUFFY-369 <prakarshkaushik369@gmail.com>
2025-10-30 00:37:57 +0530

    chore:remove redundant and unnecessary files

--- scripts/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Entrypoint for building TRT engines and starting ComfyUI-StreamDiffusion nodes
-
-# TODO: Add commands to build engines
-# python scripts/build_trt_engine.py
-
-# TODO: Add commands to start ComfyUI (if needed)
